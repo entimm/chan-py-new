7057254b9ef5bf81b570fe9e5820ae015840c12c
entimm <entimm@gmail.com>
2023-11-26 19:32:09 +0800

    移除通达信相关

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 
 from flask import Flask
-from controllers import chart_controller, index_controller, tdx_chart_controller
+from controllers import chart_controller, index_controller
 
 app = Flask(__name__, template_folder='web/templates', static_folder='web/public/static')
 app.register_blueprint(index_controller.blueprint)
 app.register_blueprint(chart_controller.blueprint)
-app.register_blueprint(tdx_chart_controller.blueprint)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
